61c0b89e41a233ec68c9e3f18f83794b9c147443
Derek Kaye <github@dezzanet.co.uk>
2020-05-01 09:56:54 +0100

    Initial commit

---
+++ src/main/java/uk/co/dezzanet/greensparrow/greensparrow/GreenSparrowApiApplication.java
@@ -0,0 +1,13 @@
+package uk.co.dezzanet.greensparrow.greensparrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenSparrowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenSparrowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/co/dezzanet/greensparrow/greensparrow/GreenSparrowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.dezzanet.greensparrow.greensparrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenSparrowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
