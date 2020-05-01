044d216f6475956820ff3b2965a439afd83caab4
Derek Kaye <github@dezzanet.co.uk>
2020-05-01 10:00:37 +0100

    reformat files

--- src/main/java/uk/co/dezzanet/greensparrow/greensparrow/GreenSparrowApiApplication.java
+++ src/main/java/uk/co/dezzanet/greensparrow/greensparrow/GreenSparrowApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GreenSparrowApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GreenSparrowApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GreenSparrowApiApplication.class, args);
+    }
 
 }
--- src/test/java/uk/co/dezzanet/greensparrow/greensparrow/GreenSparrowApiApplicationTests.java
+++ src/test/java/uk/co/dezzanet/greensparrow/greensparrow/GreenSparrowApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GreenSparrowApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
