8846b873b407af425960517d7e3a9fe09a7f9ee9
Derek Kaye <github@dezzanet.co.uk>
2020-05-15 14:17:23 +0100

    Disable CSRF

--- src/main/java/uk/co/dezzanet/greensparrow/greensparrow/config/WebSecurityConfiguration.java
+++ src/main/java/uk/co/dezzanet/greensparrow/greensparrow/config/WebSecurityConfiguration.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http.cors();
     }
 
